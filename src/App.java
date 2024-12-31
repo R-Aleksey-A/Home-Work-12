352883257e040033d4a48793eef815a08ea38381
huawei <romanec.aleksey@gmail.com>
2024-12-31 19:36:46 +0400

    HW12

---
+++ src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Home Work-12 (Library reference");
+        Author pushkin = new Author("Pushkin", "Alexandr");
+        System.out.print("Автор -" + pushkin.getLastName());
+        System.out.println(" " + pushkin.getFirstName());
+        Author tolstoi = new Author("Tolstoi", "Lion");
+        System.out.print("Автор -" + tolstoi.getLastName());
+        System.out.println(" " + tolstoi.getFirstName());
+
+
+        Book fairyTales = new Book("Fairy tales", "Pushkin", 1830);
+        System.out.println(fairyTales.getTitle() + " " + fairyTales.getAuthor() + " " + pushkin.getFirstName() + " " + fairyTales.getPublicationYear());
+        Book warAndPeace = new Book("War and Peace", "Tolstoi", 1863);
+        warAndPeace.setPublicationYear(1869);
+        System.out.println(warAndPeace.getTitle() + " " + warAndPeace.getAuthor() + " " + tolstoi.getFirstName() + " " + warAndPeace.getPublicationYear());
+
+    }
+}
---
+++ src/Author.java
@@ -0,0 +1,17 @@
+public class Author {
+    private String lastName;
+    private String firstName;
+
+    public Author(String lastName, String firstName) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+}
